57a5e40603b9056f4a9fe86c3eb96f7311ecf6df
tdudipalla <87718766+tdudipalla@users.noreply.github.com>
2024-02-07 15:38:00 -0500

    testing

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -3,8 +3,12 @@ import React from "react";
 
 const Dashboard = () => {
   return <div>
-    Welcome to StudentHealth Insight!
+    Welcome to StudentHealth Insight
     </div>;
+
+    // mission statement
+    // what each tab does 
+    // fun fact
 };
 
 export default Dashboard;
