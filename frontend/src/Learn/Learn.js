108235836a7f9200d5dd990902661550f789256a
mayajenk <92805893+mayajenk@users.noreply.github.com>
2024-02-10 20:12:22 -0500

    warning added

--- frontend/src/Learn/Learn.js
+++ frontend/src/Learn/Learn.js
@@ -30,6 +30,8 @@ const Learn = () => {
   return (
     <div>
       <h1>Common Illnesses</h1>
+      <h2>This is not a complete list. If your are experiencing symptoms, please SEE A MEDICAL PROFESSIONAL for more information!</h2>
+      <h2>See our Nearby Help page to find medical professionals near you :)</h2>
       <p>Click on an illness for more information!</p>
       <FilteredList items={data} />
     </div>
