3614f62775b22b7ac3cccd2554ba56af195ddf1f
kmperera <80276647+kmperera@users.noreply.github.com>
2024-02-26 20:36:48 -0500

    profile disabling changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import './navbarStyle.css';
