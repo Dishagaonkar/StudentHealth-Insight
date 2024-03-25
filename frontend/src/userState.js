ad93655a4a80d48178b1b2ae64fa933be3eba699
kmperera <80276647+kmperera@users.noreply.github.com>
2024-03-25 10:46:51 -0400

    changes

--- frontend/src/userState.js
+++ frontend/src/userState.js
@@ -2,9 +2,9 @@ import { set } from 'mongoose';
 import { useState } from 'react';
 
 // Initial state
-const [res, setRes] = useState("empty");
 
 // Custom hook to manage user state
 export const updateRes = (newRes) => { 
+  const [res, setRes] = useState("empty");
   setRes(newRes);
 }
