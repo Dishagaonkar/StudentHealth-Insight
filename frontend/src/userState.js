a67351b3b548208a9bdd5f8f97fbc499396ee5a7
kmperera <80276647+kmperera@users.noreply.github.com>
2024-03-25 11:26:43 -0400

    console logs

--- frontend/src/userState.js
+++ frontend/src/userState.js
@@ -5,9 +5,11 @@ export const useUserState = () => {
   const [res, setRes] = useState("empty");
 
   const updateRes = (newRes) => {
+    console.log("newRes: ", newRes);
     setRes(newRes);
   };
 
+  console.log("res: ", res);
   return {
     res,
     setRes,
