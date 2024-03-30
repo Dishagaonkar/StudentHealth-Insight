5078df00378e40f6685a550321d67a0bd016eb83
kmperera <80276647+kmperera@users.noreply.github.com>
2024-03-30 13:23:58 -0400

    Add logout functionality and fixed small text issues

---
+++ frontend/src/Logout/Logout.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import "../popup.css";
+import "../Logout/Logout.css";
+import axios from "axios";
+
+// create res outside of component
+let res = "empty";
+
+export const Logout = ({ isOpen, handleClose, updateRes, updateInactive }) => {
+
+  const LogoutClick = async () => {
+
+    //set variables to empty and update inactive and close popup
+    updateRes("empty");
+    updateInactive(true);
+    handleClose();
+
+  };
+
+  return (
+    <div className={`popup ${isOpen ? "open" : ""}`}>
+      <div className="popup-content">
+        <button className="close-btn" onClick={handleClose}>
+          X
+        </button>
+        <p className="wording">Logout</p>
+        <p className="question">Are you sure you want to logout?</p>
+          <br />
+          <div className={"buttonContainer"}>
+            <input
+              className={"inputButton"}
+              onClick={LogoutClick}
+              type="submit"
+              value={"Yes, logout."}
+            />
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logout;
