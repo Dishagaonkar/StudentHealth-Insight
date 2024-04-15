25f9502355d37bfea2af679350eb0770273856b9
kmperera <80276647+kmperera@users.noreply.github.com>
2024-04-15 09:39:40 -0400

    color

--- frontend/src/NavBar/NavBar.js
+++ frontend/src/NavBar/NavBar.js
@@ -76,17 +76,17 @@ const NavBar = () => {
           <Nav.Link as={Link} to={"evaluate"} state={{ data }}>Evaluate</Nav.Link>
         </Nav>
         <Nav style={{marginLeft:'10px', marginRight:'10px'}}>
-          <Button variant="secondary" style={{marginRight:'10px', float:'right', marginBottom:'5px', marginTop:'5px'}} onClick={(e) => {
+          <Button variant="secondary" style={{marginRight:'10px', float:'right', marginBottom:'5px', marginTop:'5px', backgroundColor: isInactive ? 'blue' : 'gray'}} onClick={(e) => {
             if (isInactive) {
               handleOpenPopUp();
             }
           }}>Login</Button>
-          <Button variant="secondary" style={{marginRight:'10px', float:'right', marginBottom:'5px', marginTop: '5px'}} onClick={(e) => {
+          <Button variant="secondary" style={{marginRight:'10px', float:'right', marginBottom:'5px', marginTop: '5px', backgroundColor: isInactive ? 'blue' : 'gray'}} onClick={(e) => {
             if (isInactive) {
               handleSignUpOpenPopUp();
             }
           }}>Sign Up</Button>
-          <Button variant="secondary" style={{marginRight:'10px', float:'right', marginBottom:'5px', marginTop: '5px'}} onClick={(e) => {
+          <Button variant="secondary" style={{marginRight:'10px', float:'right', marginBottom:'5px', marginTop: '5px', backgroundColor: isInactive ? 'gray' : 'blue'}} onClick={(e) => {
             if(!isInactive){
               handleOpenLogout();
             }
